d6a5770907c147411c22a69c92cd058fbfaa2902
IsaLaLa <thessan.l.93@gmail.com>
2019-02-16 17:00:27 +0100

    Comment

--- app/src/main/java/com/example/thereselarsson/da401a_assignment_1/MainActivity.java
+++ app/src/main/java/com/example/thereselarsson/da401a_assignment_1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.thereselarsson.da401a_assignment_1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//HEJ
 public class MainActivity extends AppCompatActivity {
 
     @Override
