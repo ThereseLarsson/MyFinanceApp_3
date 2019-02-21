05312128f9b0ab911be9bdeb088a08071ee78b33
IsaLaLa <thessan.l.93@gmail.com>
2019-02-21 17:45:31 +0100

    Personal greeting in main menu works!

--- app/src/main/java/com/example/thereselarsson/da401a_assignment_1/MainActivity.java
+++ app/src/main/java/com/example/thereselarsson/da401a_assignment_1/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.thereselarsson.da401a_assignment_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 //Main Menu in the app
 public class MainActivity extends AppCompatActivity {
+    private TextView greeting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initiateComponents();
+        updatePersonalGreeting();
+    }
+
+    public void initiateComponents() {
+        greeting = findViewById(R.id.main_greeting_1);
+    }
+
+    public void updatePersonalGreeting() {
+        String name = Startup.db.getPersonName();
+        greeting.setText("Welcome " + name + "!");
     }
 
     @Override
