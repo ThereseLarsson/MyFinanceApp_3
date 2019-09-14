bc95793a6d25d78917743aded695fb1d2f1f2e31
IsaLaLa <thessan.l.93@gmail.com>
2019-09-14 21:04:14 +0200

    Funktionalitet klar: Lista visar rätt ikoner & kan toggla mellan income/outcome i listan (ViewTransactionFragment)

--- app/src/main/java/com/example/thereselarsson/da401a_assignment_1/ViewTransactionFragment.java
+++ app/src/main/java/com/example/thereselarsson/da401a_assignment_1/ViewTransactionFragment.java
@@ -18,11 +18,25 @@ import java.util.ArrayList;
 public class ViewTransactionFragment extends Fragment {
     private View rootView;
     private ListView listView;
+    private boolean isIncome; //if false --> = outcome
+    private ArrayList<Item> itemArrayList;
+    private CustomListAdapter customListAdapter;
+    private Item item;
+    private ArrayList<Item> items;
+
+    //variables for storing data from database
     private String[] income_itemTitleList = {}; //for storing the each items title
     private String[] outcome_itemTitleList = {};
+    private String[] income_itemDateList = {}; //for storing the each items date
+    private String[] outcome_itemDateList = {};
+    private String[] income_itemAmountList = {}; //for storing the each items amount (price)
+    private String[] outcome_itemAmountList = {};
+    private String[] income_itemCategoryList = {}; //for storing the each items category
+    private String[] outcome_itemCategoryList = {};
+
+    //variables for handling icons
     private int[] income_itemIconList = {}; //for storing the each items icon
     private int[] outcome_itemIconList = {};
-    private boolean isIncome; //if false --> = outcome
 
     //variables for UI
     private TextView headline;
@@ -67,14 +81,14 @@ public class ViewTransactionFragment extends Fragment {
      * ---------------------------------------------------------------------------------------
      */
     public void initiateCustomListAdapter() {
-        ArrayList<Item> itemArrayList = generateItemsList();
-        CustomListAdapter customListAdapter = new CustomListAdapter(MainActivity.context, itemArrayList);
+        itemArrayList = generateItemsList();
+        customListAdapter = new CustomListAdapter(MainActivity.context, itemArrayList);
         listView = rootView.findViewById(R.id.viewTransaction_list);
         listView.setAdapter(customListAdapter);
     }
 
     public ArrayList<Item> generateItemsList() {
-        ArrayList<Item> items = new ArrayList<Item>();
+        items = new ArrayList<Item>();
 
         /*
         row 0 - id
@@ -85,27 +99,67 @@ public class ViewTransactionFragment extends Fragment {
          */
         income_itemTitleList = Startup.db.getIncomeValuesFromRowNbr(1);
         outcome_itemTitleList = Startup.db.getOutcomeValuesFromRowNbr(1); //is created here but not used as income is the default
+        income_itemCategoryList = Startup.db.getIncomeValuesFromRowNbr(4);
+        outcome_itemCategoryList = Startup.db.getOutcomeValuesFromRowNbr(4); //is created here but not used as income is the default
+
+        //not used here but need to be created
+        income_itemDateList = Startup.db.getIncomeValuesFromRowNbr(2);
+        outcome_itemDateList = Startup.db.getOutcomeValuesFromRowNbr(2); //is created here but not used as income is the default
+        income_itemAmountList = Startup.db.getIncomeValuesFromRowNbr(3);
+        outcome_itemAmountList = Startup.db.getOutcomeValuesFromRowNbr(3); //is created here but not used as income is the default
 
         income_itemIconList = new int[] {R.drawable.icon_salary, R.drawable.icon_other};
-        outcome_itemIconList = new int[] {R.drawable.icon_acc, R.drawable.icon_food, R.drawable.icon_sparetime, R.drawable.icon_travel, R.drawable.icon_other, R.drawable.icon_salary}; //is created here but not used as income is the default
-
-        Item item;
+        outcome_itemIconList = new int[] {R.drawable.icon_food, R.drawable.icon_sparetime, R.drawable.icon_travel, R.drawable.icon_acc, R.drawable.icon_other, R.drawable.icon_salary}; //is created here but not used as income is the default
 
         //shows the items from income as default when the list is generated into the interface for the first time
         for(int i = 0; i < income_itemTitleList.length; i++) { //längden på listan är ekvivalent med antalet items
-            item = new Item(income_itemIconList[0], income_itemTitleList[i]);
+            if(income_itemCategoryList[i].equals("Salary")) {
+                item = new Item(income_itemIconList[0], income_itemTitleList[i]);
+            } else {
+                item = new Item(income_itemIconList[1], income_itemTitleList[i]);
+            }
             items.add(item);
         }
 
         return items;
     }
 
-    public void setItemListToIncome() {
+    public void setItemListContentToIncome() {
+        items = new ArrayList<Item>();
 
+        for(int i = 0; i < income_itemTitleList.length; i++) { //längden på listan är ekvivalent med antalet items
+            if(income_itemCategoryList[i].equals("Salary")) {
+                item = new Item(income_itemIconList[0], income_itemTitleList[i]);
+            } else {
+                item = new Item(income_itemIconList[1], income_itemTitleList[i]);
+            }
+            items.add(item);
+        }
+
+        customListAdapter = new CustomListAdapter(MainActivity.context, items);
+        listView.setAdapter(customListAdapter);
     }
 
-    public void setItemListToOutcome() {
+    public void setItemListContentToOutcome() {
+        items = new ArrayList<Item>();
 
+        for(int i = 0; i < outcome_itemTitleList.length; i++) { //längden på listan är ekvivalent med antalet items
+            if(outcome_itemCategoryList[i].equals("Food")) {
+                item = new Item(outcome_itemIconList[0], outcome_itemTitleList[i]);
+            } else if (outcome_itemCategoryList[i].equals("Leisure")) {
+                item = new Item(outcome_itemIconList[1], outcome_itemTitleList[i]);
+            } else if (outcome_itemCategoryList[i].equals("Travel")) {
+                item = new Item(outcome_itemIconList[2], outcome_itemTitleList[i]);
+            } else if (outcome_itemCategoryList[i].equals("Accommodation")) {
+                item = new Item(outcome_itemIconList[3], outcome_itemTitleList[i]);
+            } else {
+                item = new Item(outcome_itemIconList[4], outcome_itemTitleList[i]);
+            }
+            items.add(item);
+        }
+
+        customListAdapter = new CustomListAdapter(MainActivity.context, items);
+        listView.setAdapter(customListAdapter);
     }
 
     /**
@@ -126,12 +180,12 @@ public class ViewTransactionFragment extends Fragment {
                     if(isIncome) {
                         headline.setText("All outcome");
                         toggleBtn.setText("Toggle to show income instead");
-                        //TODO: show list of income items from table in database
+                        setItemListContentToOutcome();
                         isIncome = false;
                     } else {
                         headline.setText("All income");
                         toggleBtn.setText("Toggle to show outcome instead");
-                        //TODO: show list of outcome items from table in database
+                        setItemListContentToIncome();
                         isIncome = true;
                     }
                     break;
