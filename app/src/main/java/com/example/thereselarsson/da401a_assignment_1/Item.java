04a4c2e99094881da308e7b185914437703ef49e
IsaLaLa <thessan.l.93@gmail.com>
2019-09-17 14:04:27 +0200

    Moved the Item-class as own class (instead of inner private class in ViewTransactionFragment).

--- app/src/main/java/com/example/thereselarsson/da401a_assignment_1/Item.java
+++ app/src/main/java/com/example/thereselarsson/da401a_assignment_1/Item.java
@@ -2,20 +2,76 @@ package com.example.thereselarsson.da401a_assignment_1;
 
 import android.widget.ImageView;
 
+/**
+ * Class for creating an object from the item stored in the SQLite database
+ * Provides easier processing of the data
+ */
+
 public class Item {
     private int icon;
     private String title;
+    private String date;
+    private int amount;
+    private String category;
+
+    public Item(String title, String date, int amount, String category, int icon) {
+        this.title = title;
+        this.date = date;
+        this.amount = amount;
+        this.category = category;
+        this.icon = icon;
+    }
 
     public Item(int icon, String title) {
         this.icon = icon;
         this.title = title;
     }
 
-    public int getIcon() {
-        return icon;
+    /**
+     * set-methods
+     * ------------------------------------------------------------------------
+     */
+    public void setTitle(String title)  {
+        this.title = title;
     }
 
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
+    }
+
+    /**
+     * get-methods
+     * ------------------------------------------------------------------------
+     */
     public String getTitle() {
         return title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getIcon() {
+        return icon;
     }
 }
