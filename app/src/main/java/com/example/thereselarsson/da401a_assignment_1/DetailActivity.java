6e22a7d7361dd36b0e9f1407dad4bb68d09e59ef
IsaLaLa <thessan.l.93@gmail.com>
2019-10-05 18:26:22 +0200

    DetailActivity - funkar delvis! Kan ladda titel, men inget annat än så länge.

--- app/src/main/java/com/example/thereselarsson/da401a_assignment_1/DetailActivity.java
+++ app/src/main/java/com/example/thereselarsson/da401a_assignment_1/DetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.thereselarsson.da401a_assignment_1;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,61 +12,67 @@ import android.widget.TextView;
  * shows a detailed view of a transaction-item
  */
 public class DetailActivity extends AppCompatActivity {
-    private View rootView;
-    private Context mContext;
-    private Item currentItem;
     private TextView titleTxtView;
     private TextView dateTxtView;
     private TextView amountTxtView;
     private TextView categoryTxtView;
     private ImageView iconView;
 
+    private String title;
+    private String date;
+    private int amount;
+    private String category;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         initiateComponents();
+        getData();
+        setData();
     }
 
     public void initiateComponents() {
-        iconView = rootView.findViewById(R.id.detailView_icon);
-        titleTxtView = rootView.findViewById(R.id.detailView_title);
-        dateTxtView = rootView.findViewById(R.id.detailView_date);
-        amountTxtView = rootView.findViewById(R.id.detailView_amount);
-        categoryTxtView = rootView.findViewById(R.id.detailView_category);
+        iconView = findViewById(R.id.detailView_icon);
+        titleTxtView = findViewById(R.id.detailView_title);
+        dateTxtView = findViewById(R.id.detailView_date);
+        amountTxtView = findViewById(R.id.detailView_amount);
+        categoryTxtView = findViewById(R.id.detailView_category);
     }
 
-    public void setInformation(Item item) {
-        this.currentItem = item;
+    public void getData() {
+        Intent intent = getIntent(); //get the intent in the target activity
+        Bundle extras = intent.getExtras(); // gets the attached bundle from the intent
+
+        //extract the stored data from the bundle
+        title = extras.getString("ClickedItemTitle");
+        //date = extras.getString("ClickedItemDate");
+        //amount = extras.getInt("ClickedItemAmount");
+        //category = extras.getString("ClickedItemCategory");
     }
 
-    private void setItemToCurrent() {
-        if(currentItem != null) {
-            if(titleTxtView != null && dateTxtView != null && amountTxtView != null && categoryTxtView != null) {
-                //iconView.setImageResource(currentItem.getIcon());
-                titleTxtView.setText(currentItem.getTitle());
-                dateTxtView.setText(currentItem.getDate());
-                amountTxtView.setText(currentItem.getAmount());
-                categoryTxtView.setText(currentItem.getCategory());
+    private void setData() {
+        titleTxtView.setText(title);
+        //dateTxtView.setText(date);
+        //amountTxtView.setText(amount);
+        //categoryTxtView.setText(category);
 
-                //sets the icon
-                //income
-                if(categoryTxtView.getText().equals("Salary")) {
-                    iconView.setImageResource(R.drawable.icon_salary);
-                } else if(categoryTxtView.getText().equals("Other")) {
-                    iconView.setImageResource(R.drawable.icon_other);
+        //sets the icon
+        //income
+        /*if(categoryTxtView.getText().equals("Salary")) {
+            iconView.setImageResource(R.drawable.icon_salary);
+        } else if(categoryTxtView.getText().equals("Other")) {
+            iconView.setImageResource(R.drawable.icon_other);
 
-                    //outcome
-                } else if(categoryTxtView.getText().equals("Food")) {
-                    iconView.setImageResource(R.drawable.icon_food);
-                } else if(categoryTxtView.getText().equals("Leisure")) {
-                    iconView.setImageResource(R.drawable.icon_sparetime);
-                } else if(categoryTxtView.getText().equals("Travel")) {
-                    iconView.setImageResource(R.drawable.icon_travel);
-                } else if(categoryTxtView.getText().equals("Accommodation")) {
-                    iconView.setImageResource(R.drawable.icon_acc);
-                }
-            }
-        }
+            //outcome
+        } else if(categoryTxtView.getText().equals("Food")) {
+            iconView.setImageResource(R.drawable.icon_food);
+        } else if(categoryTxtView.getText().equals("Leisure")) {
+            iconView.setImageResource(R.drawable.icon_sparetime);
+        } else if(categoryTxtView.getText().equals("Travel")) {
+            iconView.setImageResource(R.drawable.icon_travel);
+        } else if(categoryTxtView.getText().equals("Accommodation")) {
+            iconView.setImageResource(R.drawable.icon_acc);
+        }*/
     }
 }
